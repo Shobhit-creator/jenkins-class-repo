e9d5c9fe8d6884d228359a3b596b33b82304562e
Shobhit Singh Pal <65528935+Shobhit-creator@users.noreply.github.com>
2021-03-14 17:50:38 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -4,3 +4,4 @@ cal
 echo "this is the end of scripting"
 echo "this is fourth line"
 echo "added webhooks for github as payload url http://ip:8080/github-api"
+echo "edit webhooks for github as payload url http://13.235.244.52:8080/github-api/"
